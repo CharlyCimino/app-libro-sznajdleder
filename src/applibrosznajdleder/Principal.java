a218018bb0c3588af7f5413c371426c0be89df74
Charly Cimino <carlosecimino@gmail.com>
2021-06-05 17:11:08 -0300

    Listar empleados con join (Pag. 130)

--- src/applibrosznajdleder/Principal.java
+++ src/applibrosznajdleder/Principal.java
@@ -18,15 +18,23 @@ public class Principal {
         String pwd = "";
         String driver = "org.sqlite.JDBC"; // Ajustado a db SQLite
         String url = "jdbc:sqlite:src/applibrosznajdleder/db/empleados.db"; // Ajustado a db SQLite
-        String sql = "SELECT * FROM emp";
+        String sql = "";
+        sql += "SELECT e.empno AS empno";
+        sql += " ,e.ename AS ename ";
+        sql += " ,e.hiredate AS hiredate ";
+        sql += " ,e.deptno AS deptno";
+        sql += " ,d.dname AS dname ";
+        sql += "FROM emp e, dept d ";
+        sql += "WHERE e.empno = d.deptno ";
 
         try ( Connection con = DriverManager.getConnection(url, usr, pwd);  PreparedStatement pstm = con.prepareStatement(sql);  ResultSet rs = pstm.executeQuery();) {
             Class.forName(driver);
             while (rs.next()) {
                 System.out.print(rs.getInt("empno") + ", ");
                 System.out.print(rs.getString("ename") + ", ");
-                System.out.print(rs.getString("hiredate") + ", "); // Como String para evitar error de parseo
-                System.out.println(rs.getInt("deptno"));
+                System.out.print(rs.getInt("deptno") + " (");
+                System.out.print(rs.getString("dname") + "), ");
+                System.out.println(rs.getString("hiredate")); // Como String para evitar error de parseo
             }
         } catch (Exception ex) {
             ex.printStackTrace();
