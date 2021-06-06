e6025feaa4575950766497ce1f37bcadd73f72c8
Charly Cimino <carlosecimino@gmail.com>
2021-06-05 23:02:28 -0300

    Facade y Cliente (Pag. 151)

---
+++ src/applibrosznajdleder/Facade.java
@@ -0,0 +1,23 @@
+package applibrosznajdleder;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public class Facade {
+
+    public Collection<DeptDTO> obtenerDepartamentos() {
+        DeptDAO deptDao = new DeptDAO();
+        return deptDao.buscarTodos();
+    }
+
+    public Collection<EmpDTO> obtenerEmpleados(int deptno) {
+        EmpDAO empDao = new EmpDAO();
+        return empDao.buscarXDept(deptno);
+    }
+
+}
